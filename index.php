c6dab322fbb445591c2c5e274c8ff73ada343af3
Jaooow <joaopedro_cc2017@hotmail.com>
2020-03-03 17:52:27 -0300

    20200303

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+        <title> Atividades - João Pedro Catarina Conçolaro </title>
+        <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css" />
+        <link href = "https://fonts.googleapis.com/icon?family=Material+Icons" rel = "stylesheet"/>
+		
+		<style>
+			a{
+				color:black;
+			}
+			a:houver{
+				text-decoration:none;
+				color:lightblue;
+			}
+			h1{
+				border-bottom: 1px solid black;
+			}
+		</style>
+	</head>
+	<body>
+		<div class = "container-fluid">
+			<h1>Página com Links dos meus exemplos</h1>
+			
+			<a href = "20200303/exemplo1.html">Exemplo 1</a> <br/>
+            <a href = "20200303/exemplo2.html">Exemplo 2</a> <br/>
+            <a href = "20200303/exemplo3.html">Exemplo 3</a> <br/>
+            <a href = "20200303/exemplo3a.html">Exemplo 3 - A</a>
+		</div>
+	</body>
+</html>
